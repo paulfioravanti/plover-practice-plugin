90df65f37020553d6674ab3a0cf4ac4df04a9db2
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-01-29 13:15:59 +0900

    Add env var meta plugin

--- plover_practice_plugin/meta.py
+++ plover_practice_plugin/meta.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 def random_number(ctx, argument):
@@ -6,4 +7,12 @@ def random_number(ctx, argument):
 
     action = ctx.new_action()
     action.text = str(number)
+    return action
+
+def get_env_var(ctx, argument):
+    shell = os.getenv("SHELL", "bash").split("/")[-1]
+    env_var = os.popen(f"{shell} -ic 'echo {argument}'").read().strip()
+
+    action = ctx.new_action()
+    action.text = env_var
     return action
